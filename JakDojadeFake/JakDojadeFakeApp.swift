c045bc51e99f24451a326b3bb3301d54b6e4172d
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-12-02 16:13:39 +0100

    Initial Commit

---
+++ JakDojadeFake/JakDojadeFakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JakDojadeFakeApp.swift
+//  JakDojadeFake
+//
+//  Created by Kamil Kozioł on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct JakDojadeFakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
